3d774a7556582307a35fca5210a7b1fe0a71b4a1
arid1995 <arid1995@mail.ru>
2017-05-23 14:12:41 +0300

    Started making recycler view

---
+++ app/src/main/java/com/dimwits/vaperoid/adapters/UserAdapter.java
@@ -0,0 +1,48 @@
+package com.dimwits.vaperoid.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dimwits.vaperoid.R;
+import com.dimwits.vaperoid.requests.entities.UserEntity;
+
+/**
+ * Created by farid on 5/23/17.
+ */
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView userPicture;
+        private final TextView username;
+        private final TextView name;
+        private final TextView about;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            userPicture = (ImageView) itemView.findViewById(R.id.profile_picture);
+            username = (TextView) itemView.findViewById(R.id.profile_username);
+            name = (TextView) itemView.findViewById(R.id.profile_name);
+            about = (TextView) itemView.findViewById(R.id.profile_about);
+        }
+
+        public void bind(UserEntity user) {
+        }
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
